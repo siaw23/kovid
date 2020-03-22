a22c5a2eb840ed376ad5d02ee025ce7bcbdf6d4c
Emmanuel Hayford <siawmensah@gmail.com>
2020-03-22 09:35:11 +0100

    Refactorisie

--- lib/kovid/request.rb
+++ lib/kovid/request.rb
@@ -11,51 +11,28 @@ module Kovid
     class << self
       require 'pry'
       def by_country(country_name)
-        path = "/countries/#{country_name}"
-        fetch_url = BASE_URL + path
+        fetch_country(country_name)
 
-        response ||= JSON.parse(Typhoeus.get(fetch_url.to_s, cache_ttl: 3600).response_body)
         Kovid::Tablelize.country_table(response)
       rescue JSON::ParserError
         no_case_in(country_name)
       end
 
       def by_country_full(country_name)
-        path = "/countries/#{country_name}"
-        fetch_url = BASE_URL + path
+        fetch_country(country_name)
 
-        response ||= JSON.parse(Typhoeus.get(fetch_url.to_s, cache_ttl: 3600).response_body)
         Kovid::Tablelize.full_country_table(response)
       rescue JSON::ParserError
         no_case_in(country_name)
       end
 
       def by_country_comparison(list)
-        array = []
-
-        list.each do |country|
-          path = "/countries/#{country}"
-          fetch_url = BASE_URL + path
-
-          array << JSON.parse(Typhoeus.get(fetch_url.to_s, cache_ttl: 3600).response_body)
-        end
-
-        array = array.sort_by { |json| -json['cases'] }
-
+        array = fetch_countries(list)
         Kovid::Tablelize.compare_countries_table(array)
       end
 
       def by_country_comparison_full(list)
-        array = []
-
-        list.each do |country|
-          path = "/countries/#{country}"
-          fetch_url = BASE_URL + path
-
-          array << JSON.parse(Typhoeus.get(fetch_url.to_s, cache_ttl: 3600).response_body)
-        end
-        array = array.sort_by { |json| -json['cases'] }
-
+        array = fetch_countries(list)
         Kovid::Tablelize.compare_countries_table_full(array)
       end
 
@@ -74,6 +51,26 @@ module Kovid
         rows << ["Thankfully, there are no reported cases in #{country.capitalize}!"]
         table = Terminal::Table.new headings: [country.capitalize.to_s], rows: rows
         puts table
+      end
+
+      def fetch_countries(list)
+        array = []
+
+        list.each do |country|
+          path = "/countries/#{country}"
+          fetch_url = BASE_URL + path
+
+          array << JSON.parse(Typhoeus.get(fetch_url.to_s, cache_ttl: 3600).response_body)
+        end
+
+        array = array.sort_by { |json| -json['cases'] }
+      end
+
+      def fetch_country(country_name)
+        path = "/countries/#{country_name}"
+        fetch_url = BASE_URL + path
+
+        response ||= JSON.parse(Typhoeus.get(fetch_url.to_s, cache_ttl: 3600).response_body)
       end
     end
   end
