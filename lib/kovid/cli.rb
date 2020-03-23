db547fb2fef0695bb8be8edf69eadc533b2bdb91
Emmanuel Hayford <siawmensah@gmail.com>
2020-03-23 06:58:37 +0100

    Few fixes (#25)
    * Fix caching
    
    * Remove pry

--- lib/kovid/cli.rb
+++ lib/kovid/cli.rb
@@ -9,37 +9,37 @@ module Kovid
 
     desc 'define', 'Defines COVID-19'
     def define
-      puts Kovid::Nineteen.whatis
+      puts Kovid.whatis
     end
 
     desc 'check COUNTRY', 'Returns reported data on provided country'
     method_option :full, aliases: '-f'
     def check(name)
       if options[:full]
-        puts Kovid::Nineteen.country_full(name)
+        puts Kovid.country_full(name)
       else
-        puts Kovid::Nineteen.country(name)
+        puts Kovid.country(name)
       end
     end
 
     desc 'state STATE', 'Return reported data on provided state'
     def state(state)
-      puts Kovid::Nineteen.state(state)
+      puts Kovid.state(state)
     end
 
     desc 'compare COUNTRY COUNTRY', 'Returns full comparison table for given countries. Accepts multiple countries'
     def compare(*name)
       if FULL_FLAG.include?(name.fetch(-1))
         name = name.reverse.drop(1).reverse
-        puts Kovid::Nineteen.country_comparison_full(name)
+        puts Kovid.country_comparison_full(name)
       else
-        puts Kovid::Nineteen.country_comparison(name)
+        puts Kovid.country_comparison(name)
       end
     end
 
     desc 'cases', 'Returns total number of cases, deaths and recoveries'
     def cases
-      puts Kovid::Nineteen.cases
+      puts Kovid.cases
     end
   end
 end
