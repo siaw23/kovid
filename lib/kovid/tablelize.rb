d7576010410e3ec61dfad7aeb5726a3609512a0d
Emmanuel Hayford <siawmensah@gmail.com>
2020-03-24 14:33:08 +0100

    Add note (#33)
    * Post note on Active value
    
    * Update note

--- lib/kovid/tablelize.rb
+++ lib/kovid/tablelize.rb
@@ -67,8 +67,11 @@ module Kovid
 
         rows = []
         rows << [state['cases'], state['todayCases'], state['deaths'], state['todayDeaths'], state['recovered'], state['active']]
+        
         puts
         puts "‼️  Swap value of 'Recovered' for 'Active'. API scraper broke."
+        puts "‼️  So 'Active' is #{state['recovered']} and not #{state['active']}."
+
         Terminal::Table.new(title: state['state'], headings: headings, rows: rows)
       end
 
