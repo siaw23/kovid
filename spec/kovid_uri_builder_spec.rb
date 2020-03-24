c7e5e49dbdb9f989126d65506182cea7a461e836
Emmanuel Hayford <siawmensah@gmail.com>
2020-03-24 13:49:22 +0100

    Refactor (#31)
    Closes #1

---
+++ spec/kovid_uri_builder_spec.rb
@@ -0,0 +1,7 @@
+RSpec.describe Kovid::UriBuilder do
+  it 'returns a string URL given a path' do
+    builder = Kovid::UriBuilder.new('/hello').url
+
+    expect(builder).to eq("https://corona.lmao.ninja/hello")
+  end
+end
