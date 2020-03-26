aa7c379b50b7dbf8fa766829d08b900054988061
Emmanuel Hayford <siawmensah@gmail.com>
2020-03-26 15:14:11 +0100

    Add some tests

--- spec/kovid_uri_builder_spec.rb
+++ spec/kovid_uri_builder_spec.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 RSpec.describe Kovid::UriBuilder do
   it 'returns a string URL given a path' do
     builder = Kovid::UriBuilder.new('/hello').url
 
-    expect(builder).to eq("https://corona.lmao.ninja/hello")
+    expect(builder).to eq('https://corona.lmao.ninja/hello')
   end
 end
