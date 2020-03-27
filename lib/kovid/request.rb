0cacaf9a2aa1ea5a7ed65ca6cb2c46187e789d5f
Emmanuel Hayford <siawmensah@gmail.com>
2020-03-27 01:41:55 +0100

    Update

--- lib/kovid/request.rb
+++ lib/kovid/request.rb
@@ -15,12 +15,12 @@ module Kovid
       def eu_aggregate
         countries_array = JSON.parse(Typhoeus.get(UriBuilder.new('/countries').url, cache_ttl: 900).response_body)
 
-        eu = countries_array.select do |hash|
+        eu_array = countries_array.select do |hash|
           EU_ISOS.include?(hash['countryInfo']['iso2'])
         end
 
-        head, *tails = eu
-        eu_data = head.merge(*tails) do |key, left, right|
+        head, *tail = eu_array
+        eu_data = head.merge(*tail) do |key, left, right|
           left ||= 0
           right ||= 0
 
